7e5e527952927db14a0682d2c5d6a9640832b958
VandelayUtd <drewhadley.guitar@gmail.com>
2021-12-02 19:46:06 -0500

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,6 +1,38 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
  
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+    restaurants: [],
+    reviews: [],
+}, action) {
+    console.log(state)
+    switch(action.type){
+        case "ADD_RESTAURANT":
+            const restaurant = {
+                text: action.restaurantText,
+                id: cuid()
+            }
+            return {...state, restaurants:[...state.restaurants, restaurant]}
+
+        case "DELETE_RESTAURANT":
+            const updatedRestaurants = state.restaurants.filter(restaurant => restaurant.id !== action.restaurantId)
+            return {...state, restaurants: updatedRestaurants}
+
+        case "ADD_REVIEW":
+            const review = { 
+                text: action.review.text,
+                id: cuid(),
+                restaurantId: action.review.restaurantId
+            }
+            return {...state, reviews: [...state.reviews, review]}
+
+        case "DELETE_REVIEW":
+            const reviews = state.reviews.filter(review => review.id !== action.reviewId)
+            return {...state, reviews: reviews}
+
+        default: 
+            return state
+    }
+
 
 }
